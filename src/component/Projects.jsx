face27b52b3cfd29eae8cd8dd51448b304f9fe5c
RashedujjamanNoor <noorrk042@gmail.com>
2024-07-31 13:35:30 +0600

    add projects videos

--- src/component/Projects.jsx
+++ src/component/Projects.jsx
@@ -6,54 +6,54 @@ export const Projects = () => {
       <h1 className="text-lg font-bold text-center text-gray-800 md:text-4xl border-b-2 ">
         Projects Category
       </h1>
-      <div className="flex flex-col justify-center items-center md:grid grid-cols-4 gap-3 mt-4">
-        <div className="border-solid border-2 border-l-indigo-100 rounded-lg p-4 w-80 shadow-lg hover:scale-105 duration-100 cursor-pointer md:w-96">
+      <div className="flex flex-col justify-between items-center md:grid md:grid-cols-2 place-items-center gap-6 mt-4">
+        <div className="border-solid border-2 border-l-indigo-100 rounded-lg p-4 w-11/12 shadow-lg hover:scale-105 duration-100 cursor-pointer  md:w-full  md:flex justify-between items-center gap-3">
           <div className="flex justify-center items-center">
-            <img
-              src="../../public/images/projects/talking head.png"
-              alt=""
-              className="w-full object-cover rounded-lg"
-            />
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/tV-_9N7UVpw?si=S_PbgILHt6YcTNcz"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerpolicy="strict-origin-when-cross-origin"
+              allowfullscreen
+            ></iframe>
           </div>
-          <h1 className="font-bold text-lg text-center my-1 text-gray-800 md:text-3xl">
-            Talking Head Video
-          </h1>
+          <div>
+            <h1 className="font-bold text-lg  my-1 text-gray-800 ">
+              Client: Any Adams
+            </h1>
+            <p className="text-justify">
+              <span className="font-bold">Description:</span> Lorem, ipsum dolor
+              sit amet consectetur adipisicing elit. Dolores, enim eveniet?
+              Aliquam minus eum iste id quod qui iusto, totam
+            </p>
+          </div>
         </div>
-        <div className="border-solid border-2 border-l-indigo-100 rounded-lg p-4 w-80 shadow-lg hover:scale-105 duration-100 cursor-pointer md:w-96">
+        <div className="border-solid border-2 border-l-indigo-100 rounded-lg p-4 w-11/12 shadow-lg hover:scale-105 duration-100 cursor-pointer  md:w-full  md:flex justify-between items-center gap-3">
           <div className="flex justify-center items-center">
-            <img
-              src="../../public/images/projects/talking head.png"
-              alt=""
-              className="w-full object-cover rounded-lg"
-            />
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/tV-_9N7UVpw?si=S_PbgILHt6YcTNcz"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerpolicy="strict-origin-when-cross-origin"
+              allowfullscreen
+            ></iframe>
           </div>
-          <h1 className="font-bold text-lg text-center my-1 text-gray-800 md:text-3xl">
-            Talking Head Video
-          </h1>
-        </div>
-        <div className="border-solid border-2 border-l-indigo-100 rounded-lg p-4 w-80 shadow-lg hover:scale-105 duration-100 cursor-pointer md:w-96">
-          <div className="flex justify-center items-center">
-            <img
-              src="../../public/images/projects/talking head.png"
-              alt=""
-              className="w-full object-cover rounded-lg"
-            />
+          <div>
+            <h1 className="font-bold text-lg  my-1 text-gray-800 ">
+              Client: Marshal
+            </h1>
+            <p className="text-justify">
+              <span className="font-bold">Description:</span> Lorem, ipsum dolor
+              sit amet consectetur adipisicing elit. Dolores, enim eveniet?
+              Aliquam minus eum iste id quod qui iusto, totam
+            </p>
           </div>
-          <h1 className="font-bold text-lg text-center my-1 text-gray-800 md:text-3xl">
-            Talking Head Video
-          </h1>
-        </div>
-        <div className="border-solid border-2 border-l-indigo-100 rounded-lg p-4 w-80 shadow-lg hover:scale-105 duration-100 cursor-pointer md:w-96">
-          <div className="flex justify-center items-center">
-            <img
-              src="../../public/images/projects/talking head.png"
-              alt=""
-              className="w-full object-cover rounded-lg"
-            />
-          </div>
-          <h1 className="font-bold text-lg text-center my-1 text-gray-800 md:text-3xl">
-            Talking Head Video
-          </h1>
         </div>
       </div>
     </div>
